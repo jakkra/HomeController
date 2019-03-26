73ec77789818681eb42a2ea26f363278644992a4
Jakob Krantz <ijakkra@gmail.com>
2019-03-26 22:15:11 +0100

    Use dark background when it's night

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import HomeController from './HomeController'
 import 'moment/locale/sv';
 
+if (new Date().getHours() > 20 || new Date().getHours() < 6) {
+  document.body.style = 'background: #1e1e1e;';
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomeController/>
+        <HomeController />
       </div>
     );
   }
 }
-
 export default App;
